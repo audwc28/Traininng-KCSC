6c5a0f316d4236093e07f5f279435707fe3ddb90
audwc28 <auducis28@gmail.com>
2023-02-08 15:57:55 +0700

    upload hello

---
+++ Assembly/Hello Word/hello.asm
@@ -0,0 +1,16 @@
+section	.text
+	global _start ;khai báo để sử dụng gcc
+_start:                     ;int _start
+    mov eax, 0x04 ; gọi hàm sys_write
+    mov ebx, 1 ; gán 1 số nguyên cho biến ebx
+    mov ecx, mess ; gán biến cho thanh ghi ecx
+	mov	edx, leng ; gán độ dài biến cho thanh ghi edx
+	int	0x80   ; thực thi hàm
+	
+	mov	eax, 1 ; gọi hàm sys_exit
+	int	0x80 ; thực thi hàm
+
+section	.data
+
+    mess db	'Hello, world!', 0xa ; giống như string str = 'hello, word!'
+    leng equ	$ - mess ; Gán độ dài chuỗi mess vào len
