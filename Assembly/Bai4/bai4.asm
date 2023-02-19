e6339196320cc318c7297ab4ffc43eb4f304074e
audwc28 <auducis28@gmail.com>
2023-02-20 00:36:31 +0700

    update

--- Assembly/Bai4/bai4.asm
+++ Assembly/Bai4/bai4.asm
@@ -1,89 +1,123 @@
-section .text
-    global _start
-section .bss                         ;block starting symbol
-    num1 resb 10
-    num2 resb 10    
 section .data
+   str1 db 'Nhap so thu nhat: ', 10, 0 
+   len1 equ $ - str1
+   str2 db 'Nhap so thu hai: ', 10, 0
+   len2 equ $ - str2
+   error db 'Nhap so sai '
+   len3 equ $ - error
+   newnum db ' ', 10, 0 ; Lưu tổng của num1 + num2
+   output db 10 dup(0) ; Lưu từng chữ số của tổng vào
+   temp db ' ', 10, 0
+section .bss
+   num1 resb 10
+   num2 resb 10
+section .text
+   global _start
 _start:
-    ; Nhập số thứ nhất
-    mov ecx, num1      
-    inc ecx
-    mov edx,10
-    mov eax, 0x03
-    mov ebx, 0
-    int 0x80
-    ;Nhập số thứ hai
-    mov ecx, num2   
-    mov edx, 10
-    mov eax, 0x03
-    mov ebx, 0
-    int 0x80
-    
-    mov ecx, num1 ; Gán chuỗi trong num1 cho ecx 
-    mov al,[ecx] ; Xét tuwnngf ký tự trong chuõi
-    add al,'0' ; Cộng từng ký tự cho '0' hay 48
-    mov [ecx],al ; Gán lại tronng chuỗi của ecx
-    mov edx, num2 ; Gán chuỗi trong num2 cho edx 
-    dec edx ; edx--
-    call cong ; gọi hàm cộng
-    call to0 ; gọi hàm to0
-    
-    mov ecx, num1
-    mov edx, 10
-    mov eax, 0x04
-    mov ebx, 0
-    int 0x80
+   ;In thông báo nhập chuỗi thứ nhất
+   mov edx, len1
+   mov ecx, str1
+   mov ebx, 1
+   mov eax, 4
+   int 0x80
 
-    mov eax, 0x01
-    int 0x80
-cong: 
-    inc ecx ; ecx++
-    mov al, [ecx] ; gán từng ký tự chuỗi trong ecx cho al
-    cmp al, 0xa ; so sánh ký tự với rỗng
-    je done ; nếu đúng kết thúc chương trình
-    sub al,'0' ; al - '0' sẽ trả về số nguyên cho ký tự đó
-    inc edx ; edx ++
-    mov bl, [edx] ; gán từng ký tự chuỗi edx cho bl
-    sub bl,'0' ; bl - '0' sẽ trả về số nguyên cho ký tự đó
-    add al, bl ; al += bl;
-    cmp al, 9 ; so sánh al với 9
-    ja lon_hon_9 ; Nếu lớn hơn 9 thì gọi hàm lon_hon_9
-    add al, '0' ; al - '0' sẽ trả về kiểu str 
-    mov [ecx], al ; Gán ký tự al vào chuỗi ecx
-    jmp cong ; thực hiện ký tự tiếp theo
-    ret   
+   ;Nhập chuỗi thứ nhất
+   mov edx, 10 ; Cho bằng 10 vì tối đa 31bit
+   mov ecx, num1
+   mov ebx, 0
+   mov eax, 3
+   int 0x80
 
-to0:                ;value: 0123 -> 123
-    mov ecx, num1 ; Gán chuỗi số thứ nhất cho ecx
-    mov al, [ecx] ; Gán al với từng ký tự chuỗi trong acx
-    cmp al, '0' ; So sánh al với null
-    je to_0 ; Nếu đúng gọi hàm to_0
-    ret
+; Đổi chuỗi num1 thành số nguyên
+   xor ecx, ecx
+   mov ecx, num1  ; Gán chuỗi num1 cho thanh ghi ecx
+   mov eax, 0   ; Tạo eax = 0 để tính giá trị chuỗi
+loop1:
+   cmp byte [ecx], 0  ; So sánh với ký tự null nếu đúng thì thực hiện done1
+   je done1
+   cmp byte [ecx], 0x0A ; So sánh với ký tự xuống dòng nếu đúng thì thực hiện done1
+   je done1
+   sub byte [ecx], 48 ; chuyển đổi ký tự sang số nguyên
+   imul eax, 10  ; Nhân eax với 10
+   add al, [ecx]  ; Cộng eax với số nguyên vừa được chuyển đổi
+   inc ecx    ; tăng ecx lên 1 đơn vị để thực hiện ký tự tiếp theo
+   jmp loop1  ; Thực hiện lại vòng lặp
+done1:
+   cmp eax, 2147483647 ; neu lon hon 2 mu 31 tru 1 thi exit
+   jg exit
+   mov [num1], eax  ; Lưu giá trị số nguyên mới vào biến num1
+   
+   ;In thông báo nhập chuỗi thứ hai
+   mov edx, len2
+   mov ecx, str2
+   mov ebx, 1
+   mov eax, 4
+   int 0x80
 
-to_0:
-    mov al, 0 ; al = 0
-    mov [ecx], al ; Gán lại cho vị trí chuỗi ecx đang tính với giá al
-    ret
-    
-    
-lon_hon_9:            ;value:1a2 -> 202
-    mov ebx,ecx ; ebx += ecx
-    jmp xu_ly ; gọi hàm xu li
-    ret    
-
-xu_ly:
-    sub al, 10 ; al -= 10 (Vì lớn hơn 10 nên trừ 10 ròi nhớ 1)
-    add al, '0' ; al + '0' để lấy kus tự
-    mov [ebx], al ; Gán ký tự al vào vị trí đang tính hiện tại chuỗi ebx
-    dec ebx ; ebx--
-    mov al, [ebx] ; Gán từng ký tự của chuỗi trong ebx cho al
-    add al, 1 ; al += 1
-    sub al, '0' ; al - '0' để trả về kiểu số nguyên
-    cmp al, 9 ; so sánh al với 9 
-    ja xu_ly ; Nếu lớn hơn thì thực hiện lại hàm xu_ly
-    add al, '0' ; al +'0' trả lại ký tự tương ứng số
-    mov [ebx], al ; Gán ký tự vừa rồi vào lại chuỗi ebx đang tính hiện tại
-    jmp cong ; thực hiện tiếp hàm cộng
-    ret
-done:
-    ret
+   ;Nhập số thứ 2
+   mov edx, 10 ; Cho bằng 10 vì tối đa 31bit
+   mov ecx, num2
+   mov ebx, 0
+   mov eax, 3
+   int 0x80
+   
+   
+   xor ecx, ecx ; ecx = 0
+   ; Đổi chuỗi num2 thành số nguyên
+   mov ecx, num2  ; Gán chuỗi num2 cho thanh ghi ecx
+   mov eax, 0   ; Tạo eax = 0 để tính giá trị chuỗi
+loop2:
+   cmp byte [ecx], 0  ; So sánh với ký tự null nếu đúng thì thực hiện done2
+   je done2
+   cmp byte [ecx], 0x0A ; So sánh với ký tự xuống dòng nếu đúng thì thực hiện done2
+   je done2
+   sub byte [ecx], 48 ; chuyển đổi ký tự sang số nguyên
+   imul eax, 10  ; Nhân eax với 10
+   add al, [ecx]  ; Cộng eax với số nguyên vừa được chuyển đổi
+   inc ecx    ; tăng ecx lên 1 đơn vị để thực hiện ký tự tiếp theo
+   jmp loop2  ; Thực hiện lại vòng lặp
+done2:
+	cmp eax, 2147483647 ; neu lon hon 2 mu 31 tru 1 thi exit
+   jg exit
+   mov [num2], eax  ; Lưu giá trị số nguyên mới vào biến num2
+   
+   mov eax, [num1] ; Lưu giá trị num1 vào eax
+   add eax, [num2] ; Cộng giá trị num2 vào eax
+   mov [newnum], eax ; Lưu giá trị eax vào giá trị của newnum
+   
+   ; Đẩy từng số nguyên vào stack
+   mov eax, [newnum] ; Gán eax bằng với giá trị newnum
+   mov ebx, 10 ; Gán ebx bằng 10 để chia
+   mov edi, 0 ; gán edi = 0 để đếm số lần chia
+loop3:
+   xor edx, edx ; cho edx về 0 để lưu số dư
+   div ebx ; Lấy eax chia cho ebx phân nguyên được lưu vào eax phân dư được lưu vào edx
+   push edx ; đẩy edx lên stack
+   inc edi ; tăng biến đếm lên 1
+   cmp eax, 0 ; so sánh eax với 0 nếu khác thì thực hiện tiếp vòng lặp
+   jnz loop3
+   
+   mov [temp], edi
+   mov esi, output ; đưa địa chỉ của mảng vào thanh ghi esi
+loop4:
+   xor edx, edx ; đặt thanh ghi EDX bằng 0 để tính lại giá trị số
+   pop edx ; lấy phần dư từ stack vào thanh ghi edx
+   dec edi ; Vì edi đã lưu số lần chia nên bây giờ giảm 1 đơn vị. Khi nào về 0 thì dừng
+   add dl, '0' ; chuyển giá trị phần dư thành ký tự ASCII tương ứng
+   mov byte [esi], dl  ; lưu ký tự vào mảng
+   inc esi ; tăng con trỏ mảng lên 1
+   cmp edi, 0 ; kiểm tra xem edi đã về 0 chưa nếu chưa thì thực hiện tiếp vòng lặp
+   jnz loop4
+   
+   ; In ra chuỗi kết quả
+   mov eax, 4
+   mov ebx, 1
+   lea ecx, [output] ; đưa địa chỉ của chuỗi số vào thanh ghi ecx
+   mov edx, [temp]
+   int 0x80
+   
+exit:   ;Kết thúc chương trình
+   mov eax, 1
+   xor ebx, ebx
+   int 0x80
+   
